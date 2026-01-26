6fcd0a41a15e5d35d5bedd818a59db97e9f8a3c6
Вадим Скляров <>
2026-01-26 12:49:00 +0300

    свагер

--- catalog.api/DatabaseContext/Models/TagModel.cs
+++ catalog.api/DatabaseContext/Models/TagModel.cs
@@ -1,5 +1,6 @@
 namespace Catalog.Api.DatabaseContext.Models;
 
+using System.ComponentModel;
 using Infrastructure.Core.Abstractions;
 
 ///<inheritdoc/>
@@ -8,25 +9,30 @@ public class TagModel : IEntity<long>, IUpdated
     /// <summary>
     /// Идентификатор.
     /// </summary>
+    [Description("Идентификатор")]
     public long Id { get; set; }
 
     /// <summary>
     /// Имя.
     /// </summary>
+    [Description("Имя")]
     public string Name { get; set; } = default!;
 
     /// <summary>
     /// Кто редактировал.
     /// </summary>
+    [Description("Кто редактировал")]
     public Guid? Updated { get; set; }
     
     /// <summary>
     /// Дата редактирования.
     /// </summary>
+    [Description("Дата редактирования")]
     public DateTimeOffset? UpdatedDate { get; set; }
 
     /// <summary>
     /// Продукты.
     /// </summary>
+    [Description("Продукты")]
     public List<ProductModel> Products { get; set; } = new();
 }
